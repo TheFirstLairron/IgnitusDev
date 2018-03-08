ba09afdbabfd1fa7a898778f4b6cbb22489d719c
TheFirstLairron <zrwall@hotmail.com>
2018-03-08 11:26:05 -0800

    Simple style for sidebar. Not complete

--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -4,8 +4,11 @@ import styles from './styles.css';
 
 class Sidebar extends React.Component {
     render() {
-        return(
-            <div styleName="container">SIDEBAR</div>
+        return (
+            <div styleName="container">
+                <div styleName="header">Navigation:</div>
+                <button>Stuff</button>
+            </div>
         );
     }
 }
